3a45b3d5aa431cc8976ef5bf70c1b21ddc997689
chungcha <charlieschung@gmail.com>
2020-01-14 18:55:59 -0500

    installed fastjsonapi and generated serializers

--- simpleSockets/simple_sockets_backend/config/routes.rb
+++ simpleSockets/simple_sockets_backend/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :rooms, only: [:index, :show]
   resources :messages, only: [:index, :create]
   resources :users, only: [:index, :create]
-  mount ActionCable.server = '/cable'
+  mount ActionCable.server => '/cable'
 
 end

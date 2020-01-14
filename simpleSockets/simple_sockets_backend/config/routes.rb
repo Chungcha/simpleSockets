e8e11a7e410441d102b28e8901de864a75b3e5de
chungcha <charlieschung@gmail.com>
2020-01-14 17:51:21 -0500

    did associations

--- simpleSockets/simple_sockets_backend/config/routes.rb
+++ simpleSockets/simple_sockets_backend/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
-  
+  resources :rooms, only: [:index, :show]
+  resources :messages, only: [:index, :create]
+  resources :users, only: [:index, :create]
+  mount ActionCable.server = '/cable'
 
 end
